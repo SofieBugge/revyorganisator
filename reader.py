fdb222f4dce2e511898f9f4bc1a5fb044747d191
Nicholas Christian Langkjær Ipsen <hts399@alumni.ku.dk>
2016-02-16 01:05:39 +0100

    Updated JSON-reader to version 0.21: CHANGELOG  - Actully added documentation.  - Fixed minor indentation-errors.

--- reader.py
+++ reader.py
@@ -1,6 +1,6 @@
 """Parses the JSON-database."""
 # **********************
-# * JSON-reader v. 0.2 *
+# * JSON-reader v. 0.21 *
 # **********************
 import json
 from pprint import pprint
@@ -8,8 +8,9 @@ from pprint import pprint
 with open('database.json') as data_file:
     data = json.load(data_file)
 
-"""pprint(data)
-pprint(data["acts"][0]["materials"][0]["title"])"""
+"""The menu. It let's you do stuff. Mostly justhere to play around with, and
+   is unlikely to actually be used for anything. The logic it uses should be
+   reimplemented in the final version though."""
 while True:
     print (30 * '-')
     print ("   M A I N - M E N U")
@@ -32,6 +33,8 @@ while True:
             subselection = input()
             if subselection == "1":
                 id = int(input("Please enter the ID of the act: "))
+                # Makes sure the ID entered actually exists, then info about
+                # the act.
                 if id <= (number_of_acts - 1) & id > 0:
                     print("\n")
                     pprint(data["acts"][0]["materials"][id])
@@ -43,6 +46,9 @@ while True:
             if subselection == "2":
                 name = input("\nPlease enter the title: ")
                 count = 0
+                # Converts the title entered by the user to lowercase,
+                # then checks it against every title of every act in the
+                # database.
                 for x in range(0, number_of_acts):
                     title = (data["acts"][0]["materials"][x-1]["title"])
                     if title.lower() == name.lower():
@@ -55,12 +61,14 @@ while True:
                 print("\nUnknown title. Returning to main menu.")
                 break
             elif count > 1:
-                print("\nMore than one act have the title", title,
-                ". All of them have been printed above, ordered by ID.")
+                print("\nMore than one act has the title", title,
+                      ". All of them have been printed above, ordered by ID.")
             else:
                 print("\n")
                 break
     elif selection == '3':
+        # Searches through the database, and finds all acts where a particular
+        # actor is involved. They are then presented to the user.
         print("Please enter the name of the actor:")
         name = input()
         count = 0
@@ -68,10 +76,11 @@ while True:
             number_of_roles = len(data["acts"][0]["materials"][x-1]["roles"])
             for y in range(0, number_of_roles):
                 result = (data["acts"][0]["materials"][x-1]
-                         ["roles"][y-1]["actor"])
+                          ["roles"][y-1]["actor"])
                 if result.lower() == name.lower():
                     print("\n", name, " appears in act ", x-1, ", \"",
-                    (data["acts"][0]["materials"][x-1]["title"]), "\"", sep='')
+                          (data["acts"][0]["materials"][x-1]["title"]),
+                          "\"", sep='')
                     count = count + 1
         if count == 0:
             print("\n", name, " does not appear in any acts.\n", sep='')
